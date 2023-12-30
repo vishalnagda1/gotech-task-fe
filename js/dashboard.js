06bb27683c147409ae4e4163f9077b9060613d65
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-30 15:14:47 +0530

    create display file list functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,11 +7,33 @@ window.onload = function () {
         redirect: 'follow'
     };
 
+    // Fetch the list of files
     fetch(HOST + '/list/', requestOptions)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => {
+            // Update the UI with the list of files
+            displayFileList(result.files);
+        })
         .catch(error => console.log('error', error));
 };
+
+function displayFileList(files) {
+    const fileListContainer = document.getElementById('fileList');
+
+    // Clear existing content
+    fileListContainer.innerHTML = '';
+
+    // Display each file in the list
+    files.forEach(file => {
+        const fileItem = document.createElement('div');
+        fileItem.innerHTML = `
+            <span>${file.file_name}</span>
+            <button onclick="renameFile(${file.file_id})">Rename</button>
+            <button onclick="deleteFile(${file.file_id})">Delete</button>
+        `;
+        fileListContainer.appendChild(fileItem);
+    });
+}
 
 function logout() {
     const requestOptions = {
@@ -21,10 +43,10 @@ function logout() {
     };
 
     fetch(HOST + '/signout/', requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
-        window.location.href = '/';
-    })
-    .catch(error => console.log('error', error));
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            window.location.href = '/';
+        })
+        .catch(error => console.log('error', error));
 }
