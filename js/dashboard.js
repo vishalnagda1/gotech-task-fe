23dc501d957e034562cf9f29ebaea7c9a18d3cee
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-30 15:15:26 +0530

    create rename file functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -50,3 +50,27 @@ function logout() {
         })
         .catch(error => console.log('error', error));
 }
+
+function renameFile(fileId) {
+    const newFileName = prompt('Enter the new name for the file:');
+    if (newFileName) {
+        const requestOptions = {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ new_name: newFileName }),
+            redirect: 'follow'
+        };
+
+        fetch(HOST + `/rename/${fileId}/`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                // Refresh the file list after renaming
+                window.location.reload();
+            })
+            .catch(error => console.log('error', error));
+    }
+}
